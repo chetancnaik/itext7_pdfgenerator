3f97904818f51633b1cb19d7bc3560182efaa493
chetancnaik <chetancnaik@gmail.com>
2023-02-16 12:40:09 +0530

    First draft of few examples

---
+++ src/main/java/com/domain/pdfgenerator/App.java
@@ -0,0 +1,13 @@
+package com.domain.pdfgenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/domain/pdfgenerator/converter/C01E01_HelloWorld.java
@@ -0,0 +1,51 @@
+package com.domain.pdfgenerator.converter;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.html2pdf.HtmlConverter;
+//import com.itextpdf.licensing.base.LicenseKey;
+
+public class C01E01_HelloWorld {
+	
+	/**
+	 * The path to the resulting PDF file.
+	 */
+	public static final String DEST = "./target/htmlsamples/ch01/helloWorld01.pdf";
+
+	/**
+	 * The HTML-string that we are going to convert to PDF.
+	 */
+	public static final String HTML = "<h1>Test</h1><p>Hello World</p>";
+
+	/**
+	 * The main method of this example.
+	 *
+	 * @param args no arguments are needed to run this example.
+	 * @throws IOException signals that an I/O exception has occurred.
+	 */
+	public static void main(String[] args) throws IOException {
+//		try (FileInputStream license = new FileInputStream(System.getenv("ITEXT7_LICENSEKEY")
+//				+ "/itextkey-html2pdf_typography.json")) {
+//			LicenseKey.loadLicenseFile(license);
+//		}
+		File file = new File(DEST);
+		file.getParentFile().mkdirs();
+
+		new C01E01_HelloWorld().createPdf(HTML, DEST);
+	}
+
+	/**
+	 * Creates the PDF file.
+	 *
+	 * @param html the HTML as a String value
+	 * @param dest the path of the resulting PDF
+	 * @throws IOException signals that an I/O exception has occurred.
+	 */
+	public void createPdf(String html, String dest) throws IOException {
+		HtmlConverter.convertToPdf(html, new FileOutputStream(dest));
+	}
+
+}
