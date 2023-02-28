04ca65f7a4c260fa66df3314f9b64abca48c325a
chetancnaik <chetancnaik@gmail.com>
2023-02-28 08:37:05 +0530

    Read from DOM

--- src/main/java/com/domain/pdfgenerator/converter/XmlToHtmlTransformer.java
+++ src/main/java/com/domain/pdfgenerator/converter/XmlToHtmlTransformer.java
@@ -22,14 +22,46 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 public class XmlToHtmlTransformer {
 
 	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException,
 			TransformerFactoryConfigurationError, TransformerException {
-		transform("./src/main/resources/htmlsamples/xml/report.xml", "./src/main/resources/htmlsamples/xml/stylesheet.xsl",
-				"./target/htmlsamples/ch04/report.html");
+		String xml = "./src/main/resources/htmlsamples/xml/report.xml";
+		String xslt = "./src/main/resources/htmlsamples/xml/stylesheet.xsl";
+		String pdf = "./target/htmlsamples/ch04/report.html";
+		
+//		transform(xml, xslt, pdf);
+		
+		InputStream xmlData = new FileInputStream(xml);
+		Document xmlDocument = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlData);
+		Source src = new DOMSource(xmlDocument);
+		transformFromDom(src, xslt, pdf);
+	}
+	
+	public static void transformFromDom(final Source src, final String xslt, String outputFile) throws IOException,
+		TransformerFactoryConfigurationError, TransformerException {
+	
+		URL xsltURL = new File(xslt).toURI().toURL();
+		
+		Source stylesource = new StreamSource(xsltURL.openStream(), xsltURL.toExternalForm());
+		Transformer transformer = TransformerFactory.newInstance().newTransformer(stylesource);
+		
+		StringWriter stringWriter = new StringWriter();
+		transformer.transform(src, new StreamResult(stringWriter));
+		
+		// write to file
+		File file = new File(outputFile);
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		
+		FileWriter fw = new FileWriter(file);
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(stringWriter.toString());
+		bw.close();
 	}
 
 	public static void transform(final String xml, final String xslt, String outputFile) throws SAXException, IOException,
